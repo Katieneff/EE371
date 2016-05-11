6aa2337fa7e33d90b7875b25c1099d95b2a03f6d
Katelyn E. Neff <neffk2@uw.edu>
2016-05-10 19:10:49 -0700

    SRAM

--- lab3/sram/microprocessor/microprocessor_bb.v
+++ lab3/sram/microprocessor/microprocessor_bb.v
@@ -1,20 +1,20 @@
 
 module microprocessor (
-	clk_clk,
-	reset_reset_n,
-	leds_export,
 	address_export,
+	clk_clk,
 	data_in_port,
 	data_out_port,
+	leds_export,
 	oe_export,
+	reset_reset_n,
 	we_export);	
 
-	input		clk_clk;
-	input		reset_reset_n;
-	output	[7:0]	leds_export;
 	output	[7:0]	address_export;
+	input		clk_clk;
 	input	[7:0]	data_in_port;
 	output	[7:0]	data_out_port;
+	output	[7:0]	leds_export;
 	output		oe_export;
+	input		reset_reset_n;
 	output		we_export;
 endmodule
--- lab3/sram/src/main.v
+++ lab3/sram/src/main.v
@@ -2,23 +2,34 @@ module main(CLOCK_50, LEDR, KEY);
 
 	input CLOCK_50;
 	input [0:0] KEY;
-	output [7:0] LEDR;
+	output [8:0] LEDR;
 	
 	wire [7:0] data;
+	
+	wire[7:0] data_in;
+	wire[7:0] data_out;
 	wire [10:0] address;
 	wire we, oe;
 	
+
+	//wire  clk;
+	//clockdiv clockdiv(clk, CLOCK_50);
+		//assign LEDR[8:8] = clk;
+
 	 microprocessor m(
 		 .address_export(address), // address.export
 		 .clk_clk(CLOCK_50),        //     clk.clk
-		 .data_in_port(data),   //    data.in_port
-		 .data_out_port(data),  //        .out_port
+		 .data_in_port(data_in),   //    data.in_port
+		 .data_out_port(data_out),  //        .out_port
 		.leds_export(LEDR),    //    leds.export
 		.oe_export(oe),      //      oe.export
 		.reset_reset_n(KEY),  //   reset.reset_n
 		.we_export(we)       //      we.export
 	);
 	
+	
+	assign data = (we) ? 8'bz : data_out;
+	assign data_in = (oe) ? 8'bz : data;
 	
 	sram sram(.data(data), .addrs(address), .we(we), .oe(oe), .clk(CLOCK_50), .rst(KEY));
 	
