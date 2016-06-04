dddb477420973549a799d60c418b4176bd9527bb
Katelyn E. Neff <neffk2@uw.edu>
2016-06-04 14:02:58 -0700

    refactored AF

--- battleship/software/microproc/hello_world_small.c
+++ battleship/software/microproc/hello_world_small.c
@@ -88,16 +88,27 @@
 #define character_received (volatile char *) 0x3010
 #define character_sent (volatile char *) 0x3020
 
+#define WATER_CHAR (char) 'w'
+#define MISS_CHAR (char) 'x'
+#define HIT_CHAR (char) 'm'
+#define BATTLESHIP_CHAR (char) 'b'
+#define CRUISER_CHAR (char) 'r'
+#define CARRIER_CHAR (char) 'c'
+#define SUBMARINE_CHAR (char) 's'
+#define DESTROYER_CHAR (char) 'd'
+#define NO_OP_CHAR (char) 'n'
 
-#define MISS_CHAR (char) x
-#define HIT_CHAR (char) y
-#define DOWNED_CARRIER_CHAR (char) a
-#define DOWNED_BATTLESHIP_CHAR (char) b
-#define DOWNED_CRUISER_CHAR (char) c
-#define DOWNED_SUBMARINE_CHAR (char) d
-#define DOWNED_DESTROYER_CHAR (char) e
-#define NO_OP_CHAR (char) f
+//makes counter for the number of hits on a certain ship
+/************************************/
+int carrierCounter;
+int battleshipCounter;
+int cruiserCounter;
+int submarineCounter;
+int destroyerCounter;
+/**********************/
 
+int counter;
+char gameBoard[10][10];
 
 int send(unsigned int str);
 
@@ -108,241 +119,59 @@ int checkShot(char gameBoard[10][10], int i, int j);
 void getCoordinates();
 
 /*
-int main()
-{ 
-  alt_putstr("Hello from Nios II!\n");
-  alt_putstr("Welcome to Battleship!\n");
+ int main()
+ {
+ alt_putstr("Hello from Nios II!\n");
+ alt_putstr("Welcome to Battleship!\n");
 
 
-  int character;
-  int data;
+ int character;
+ int data;
 
 
-  *transmit_enable = 1;
-  int playerNum = getPlayerNum();
+ *transmit_enable = 1;
+ int playerNum = getPlayerNum();
 
 
-  switch (playerNum) {
-  	  case '1':
-  		alt_putstr("Player 1 rules\n");
-  		playerOnePlay();
-  		break;
-  	  case '2':
-  		alt_putstr("Player 2!\n");
-  		playerTwoPlay();
-  		break;
-  }
+ switch (playerNum) {
+ case '1':
+ alt_putstr("Player 1 rules\n");
+ playerOnePlay();
+ break;
+ case '2':
+ alt_putstr("Player 2!\n");
+ playerTwoPlay();
+ break;
+ }
 
 
-  return 0;
-}*/
+ return 0;
+ }*/
 
+int main() {
 
+	alt_putstr("Welcome to Battleship!\n");
 
-int main () {
+	int playerNum = getPlayerNum();
 
-	alt_putstr("Hello from Nios II!\n");
-	char gameBoard [ 10 ][ 10 ];
-
-
-	// initialize all the variables.
-	/******************************/
-	char water      =   '~';
-	char hit        =   'x';
-	char miss       =   'm';
-	char carrier    =   'c'; // length 5
-	char battleship =   'b'; // length 4
-	char cruiser    =   'r'; // length 3
-	char submarine  =   's'; // length 3
-	char destroyer  =   'd'; // length 2
-	/********************************/
-
-	//Variable being used in the main program
-	/**********************************/
-	unsigned int h = 0;
-	unsigned int k = 0;
-	unsigned int response;
-	int shot = 0;
-	int counter = 0;
+	gameInit();
 	int keepScore = 0;
-	/************************************/
-
-	//makes counter for the number of hits on a certain ship
-	/************************************/
-	int carrierCounter = 0;
-	int battleshipCounter = 0;
-	int cruiserCounter = 0;
-	int submarineCounter = 0;
-	int destroyerCounter = 0;
-	/**********************/
-
-	int i, j;
-	/***************************
-	puts in water in the 2d array*/
-	for (i = 0; i<10; i++ ){
-		for (j = 0; j <10; j++) {
-			gameBoard[i][j] = water;
-		}
-	}
-	/*****************************/
-
-
-	/*******************************
-	 assigns all the different characters
-	 to different positions in the 2d array */
-	for (j = 0; j<5 ; j++) {
-		gameBoard [1][j] = carrier;
-	 }
-
-	for (i = 0; i< 4; i++) {
-		gameBoard [i+3][7] = battleship;
-	 }
-
-	for (i = 0; i< 3; i++) {
-		gameBoard [i+4][9] = cruiser;
-	 }
-
-	for (j = 0; j< 3; j++) {
-		gameBoard [7][1+j] = submarine;
-	 }
-
-	for (j = 0; j< 2; j++) {
-		gameBoard [9][8+j] = destroyer;
-	 }
-	/**************************************/
-
-
-	/* prints the board in the beginning of the game */
-	alt_printf("  0 1 2 3 4 5 6 7 8 9\n"); // prints the top row of number
-	for (i = 0; i<10; i++ ){
-		alt_printf("%x ",i); //prints the verticle row of number
-			for (j = 0; j <10; j++) {
-				alt_printf ("%c ", gameBoard[i][j]);
-		}
-		alt_putstr("\n");
-	 }
-
-
-	/******************************************/
 
 	do {
-
-		getCoordinates();
-
-
-		// Wait for response to hit
-		response = receive();
-
-
-		switch (response) {
-			case MISS_CHAR: 
-				alt_putstr("Miss!\n");
-				break;
-			case HIT_CHAR: 
-				alt_putstr("It was a hit! \n");
-				counter++;
-				break;
+		switch (playerNum) {
+		case '1':
+			playerOnePlay();
+			break;
+		case '2':
+			playerTwoPlay();
+			break;
 		}
-
-		// Wait for other players attack
-		h = recieve();
-		k = receive();
-
-		/*******************************************************
-		sends the data to checkShot to see if
-		the player hit, miss, or has he already used tht input*/
-
-		shot = checkShot(gameBoard, h, j);
-		/******************************************************/
-
-
-
-			
-		/***********************************
-		gets the data from the checkShot and checks
-		if the coordinates entered by the user was a hit
-		or a miss or if the user has already used those
-		coordinates */
-		switch (shot){
-			case 0: alt_putstr("Miss!\n");
-				gameBoard[h][k] = miss;
-				send(MISS_CHAR);
-				break;
-			case 1: alt_putstr("It was a hit! \n");
-				gameBoard[h][k] = hit;
-				//counter++;
-				send(HIT_CHAR);
-				break;
-			case 2: alt_putstr("Please pick another value you have already chosen that\n");
-				break;
-		}
-		/*************************************/
-
-
-
-		/*The following checks if you have hit some
-			ship and keeps track of how many times
-			the player has hit the ship */
-
-		if (gameBoard[h][k] == carrier){
-			carrierCounter ++;
-		} else if (gameBoard[h][k] == battleship) {
-			battleshipCounter++;
-		} else if (gameBoard[h][k] == cruiser) {
-			cruiserCounter++;
-		} else if (gameBoard[h][k] == submarine) {
-			submarineCounter++;
-		} else if (gameBoard[h][k] == destroyer) {
-			destroyerCounter++;
-		}
-
-
-		/****************************************
-		The follwing checks if a certain ship is hit certain
-		number of times and output the message to the user
-		saying that the have drowned that particular ship*/
-		if (carrierCounter == 5){
-			alt_putstr("You just drowned the carrier\n");
-			carrierCounter = 0;
-		} else if (battleshipCounter==4){
-			alt_putstr("You just drowned the battleship\n");
-			battleshipCounter = 0;
-		} else if (cruiserCounter==3){
-			alt_putstr("You just drowned the cruiser\n");
-			cruiserCounter = 0;
-		} else if(submarineCounter==3){
-			alt_putstr("You just drowned the submarine\n");
-			submarineCounter = 0;
-		} else if(destroyerCounter == 2){
-			alt_putstr("You just drowned the destroyer\n");
-			destroyerCounter = 0;
-		}
-		
-
-
-
-
-		/*************************************
-		just prints the board after adding the hit or miss*/ // Make method?
-	
-		alt_putstr("  0 1 2 3 4 5 6 7 8 9\n");
-
-		for (i = 0; i<10; i++ ) {
-			alt_printf("%x ",i);
-			for (j = 0; j <10; j++) {
-				alt_printf("%c ", gameBoard[i][j]);
-			}
-			alt_putstr("\n");
-		}
-	
-		/**************************************/
 
 		keepScore++; // how many turns did the player take to win
 
 	} while (counter < 17);
 
 }
-
 
 int send(unsigned int str) {
 	alt_putstr("send!\n");
@@ -363,82 +192,61 @@ int send(unsigned int str) {
 }
 
 int receive() {
+	*transmit_enable = 1;
 	alt_putstr("Receive!\n");
 	int data;
 	while (1) {
-	if (*character_received) {
-	  usleep(100);
-	  data = *data_bus_in;
+		if (*character_received) {
+			usleep(100);
+			data = *data_bus_in;
 
+			if (data & 128) {
+				data = (data / 2) & 127;
+				alt_putstr("fix\n");
+			}
 
-	  if (data & 128) {
-		  data = (data / 2) & 127;
-		  alt_putstr("fix\n");
-	  }
-
-	  return data;
-	}
+			return data;
+		}
 	}
 	return 0;
 }
-
 
 int getPlayerNum() {
 	alt_putstr("Are you player 1 or player 2? \n");
 	int num = alt_getchar();
 	return num;
 
-
 }
 
-
-int playerOnePlay(){
-	while(1)	{
-		*transmit_enable = 0;
-		alt_putstr("Send a character: ");
-		unsigned int character = alt_getchar();
-		if (character == '\n') {
-			character = alt_getchar();
-		}
-		send(character);
-		*transmit_enable = 1;
-		receive();
-	}
-	return 0;
-
-}
-
-
-int playerTwoPlay(){
-	while (1) {
-		*transmit_enable = 1;
-		receive();
-
-		*transmit_enable = 0;
-		int character = alt_getchar();
-		send(character);
-	}
+int playerOnePlay() {
+	attack();
+	alt_putstr("Player 2's Turn...")
+	getAttacked();
 	return 0;
 }
 
-
-
+int playerTwoPlay() {
+	alt_putstr("Player 1's Turn...");
+	getAttacked();
+	attack();
+	return 0;
+}
 
 void getCoordinates() {
 	unsigned int h, k;
 	/**************************************
-	the following alt_putstr asks and stores
-	players input*/
+	 the following alt_putstr asks and stores
+	 players input*/
 
-	alt_putstr ("> Enter Target:\n");
-	alt_putstr ("> Enter longitude: ");
+	alt_putstr("> Enter Target:\n");
+	alt_putstr("> Enter longitude: ");
 	h = alt_getchar();
 	if (h == '\n') {
 		h = alt_getchar();
 	}
 	h = h - 48;
 
-	alt_putstr ("> Enter latitude: ");
+	alt_putstr("> Enter latitude: ");
 	k = alt_getchar();
 	if (k == '\n') {
 		k = alt_getchar();
@@ -453,25 +261,206 @@ int checkShot(char gameBoard[10][10], int i, int j) {
 	int HIT = 0;
 
 	switch (gameBoard[i][j]) {
-		// when its a miss
-		case '~':HIT = 0;
-			break;
+	// when its a miss
+	case '~':
+		HIT = 0;
+		break;
 
 		// when its a hit
-		case 'c':
-		case 'b':
-		case 'r':
-		case 's':
-		case 'd':
-			HIT = 1;
-			break;
+	case 'c':
+	case 'b':
+	case 'r':
+	case 's':
+	case 'd':
+		HIT = 1;
+		break;
 		// when its already been used
-		case 'x':
-		case 'm':
-		default:
-			HIT = 2;
-			break;
+	case 'x':
+	case 'm':
+	default:
+		HIT = 2;
+		break;
 	}
 
 	return HIT;
+}
+
+void printBoard(char gameBoard[][]) {
+	/* prints the board in the beginning of the game */
+	int i, j;
+	alt_printf("  0 1 2 3 4 5 6 7 8 9\n"); // prints the top row of number
+	for (i = 0; i < 10; i++) {
+		alt_printf("%x ", i); //prints the vertical row of number
+		for (j = 0; j < 10; j++) {
+			alt_printf("%c ", gameBoard[i][j]);
+		}
+		alt_putstr("\n");
+	}
+
+}
+
+void attack() {
+	// Send a missle
+	getCoordinates();
+
+	// Wait for response to hit
+	unsigned int response = receive();
+
+	switch (response) {
+	case MISS_CHAR :
+		alt_putstr("Miss!\n");
+		break;
+	case CARRIER_CHAR :
+		alt_putstr("You hit the carrierer! \n");
+		counter++;
+		carrierCounter++;
+		break;
+	case BATTLESHIP_CHAR :
+		alt_putstr("You hit the battleship! \n");
+		counter++;
+		battleshipCounter++;
+		break;
+	case CRUISER_CHAR :
+		alt_putstr("You hit the cruiser! \n");
+		counter++;
+		cruiserCounter++;
+		break;
+	case SUBMARINE_CHAR :
+		alt_putstr("You hit the submarine! \n");
+		counter++;
+		submarineCounter++;
+		break;
+	case DESTROYER_CHAR :
+		alt_putstr("You hit the destroyer! \n");
+		counter++;
+		destroyerCounter++;
+		break;
+	}
+
+	/****************************************
+	 The following checks if a certain ship is hit certain
+	 number of times and output the message to the user
+	 saying that the have drowned that particular ship*/
+	if (carrierCounter == 5) {
+		alt_putstr("You just drowned the carrier\n");
+		carrierCounter = 0;
+	} else if (battleshipCounter == 4) {
+		alt_putstr("You just drowned the battleship\n");
+		battleshipCounter = 0;
+	} else if (cruiserCounter == 3) {
+		alt_putstr("You just drowned the cruiser\n");
+		cruiserCounter = 0;
+	} else if (submarineCounter == 3) {
+		alt_putstr("You just drowned the submarine\n");
+		submarineCounter = 0;
+	} else if (destroyerCounter == 2) {
+		alt_putstr("You just drowned the destroyer\n");
+		destroyerCounter = 0;
+	}
+
+}
+
+void getAttacked() {
+	// Wait for other players attack
+	unsigned int h = recieve();
+	unsigned int k = receive();
+
+	/*******************************************************
+	 sends the data to checkShot to see if
+	 the player hit, miss, or has he already used that input*/
+
+	int shot = checkShot(gameBoard, h, k);
+	/******************************************************/
+
+	/***********************************
+	 gets the data from the checkShot and checks
+	 if the coordinates entered by the user was a hit
+	 or a miss or if the user has already used those
+	 coordinates */
+	switch (shot) {
+	case 0:
+		alt_putstr("Miss!\n");
+		gameBoard[h][k] = MISS_CHAR;
+		send(MISS_CHAR);
+		break;
+	case 1:
+		alt_putstr("It was a hit! \n");
+		gameBoard[h][k] = HIT_CHAR;
+		//send(HIT_CHAR);
+		send(gameBoard[h][k]);
+		break;
+	case 2:
+		alt_putstr("Please pick another value you have already chosen that\n");
+		break;
+	}
+	/*************************************/
+
+}
+
+// Initialized board and globals
+void gameInit() {
+
+	// initialize all the variables.
+	/******************************/
+	/*char water      =   '~';
+	 char hit        =   'x';
+	 char miss       =   'm';
+	 char carrier    =   'c'; // length 5
+	 char battleship =   'b'; // length 4
+	 char cruiser    =   'r'; // length 3
+	 char submarine  =   's'; // length 3
+	 char destroyer  =   'd'; // length 2*/
+	/********************************/
+
+	//Variable being used in the main program
+	/**********************************/
+
+	counter = 0;
+
+	/************************************/
+
+	//makes counter for the number of hits on a certain ship
+	/************************************/
+	carrierCounter = 0;
+	battleshipCounter = 0;
+	cruiserCounter = 0;
+	submarineCounter = 0;
+	destroyerCounter = 0;
+	/**********************/
+
+	int i, j;
+	/***************************
+	 puts in water in the 2d array*/
+	for (i = 0; i < 10; i++) {
+		for (j = 0; j < 10; j++) {
+			gameBoard[i][j] = WATER_CHAR;
+		}
+	}
+	/*****************************/
+
+	/*******************************
+	 assigns all the different characters
+	 to different positions in the 2d array */
+	for (j = 0; j < 5; j++) {
+		gameBoard[1][j] = CARRIER_CHAR;
+	}
+
+	for (i = 0; i < 4; i++) {
+		gameBoard[i + 3][7] = BATTLESHIP_CHAR;
+	}
+
+	for (i = 0; i < 3; i++) {
+		gameBoard[i + 4][9] = CRUISER_CHAR;
+	}
+
+	for (j = 0; j < 3; j++) {
+		gameBoard[7][1 + j] = SUBMARINE_CHAR;
+	}
+
+	for (j = 0; j < 2; j++) {
+		gameBoard[9][8 + j] = DESTROYER_CHAR;
+	}
+	/**************************************/
+	printBoard(gameBoard);
+
 }
